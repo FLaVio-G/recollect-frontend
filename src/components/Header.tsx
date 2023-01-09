283c78138eb363d19aa476e3b233d16575e41b3c
FLaVio-G <flavio.andrade@orbita.srv.br>
2023-01-09 20:15:41 -0300

    Feat: Configurando projeto

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import { Flex, Heading, Text } from "@chakra-ui/react";
+
+export function Header() {
+    return(
+        <Flex>
+            <Flex>
+                <Heading>Recollect</Heading>
+                <Text>Not </Text>
+            </Flex>
+        </Flex> 
+    )
+}
