080d63873421fd9c865ae8d05a824dcd0385d3de
FLaVio-G <flaviogabrielandrade@gmail.com>
2023-01-17 02:39:23 -0300

    Fix: Correção de links e novo componente de mapa

--- src/pages/Parceiros.tsx
+++ src/pages/Parceiros.tsx
@@ -1,12 +1,14 @@
 import { Header } from "../components/Header/Header";
 import { Box } from "@chakra-ui/react";
 import logo from "../assets/imgs/morro-2.png";
+import { MapaParceiros } from "../components/googleMaps/MapaParceiros";
 
 function Parceiros() {
   return (
     <>
       <Box bgImage={logo} height="100vh">
         <Header />
+        <MapaParceiros />
       </Box>
     </>
   );
