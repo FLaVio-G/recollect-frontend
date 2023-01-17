03bccfd4882842e991f1607f3b47ad253507f39d
Felipe Dias <pdias.felipe@gmail.com>
2023-01-17 17:59:04 -0300

    Fix: RecuperarSenhaCard

--- src/components/RecuperarSenha/RecuperarSenhaCard.tsx
+++ src/components/RecuperarSenha/RecuperarSenhaCard.tsx
@@ -40,7 +40,7 @@ import {
             <Center>
                 
                 <Stack className="stack" spacing={3}  width='70%'>
-                        <Input placeholder='e-mail' type='email' width='50%'/>    
+                        <Input placeholder='e-mail' type='email'/>    
                         <Popover>
                         <PopoverTrigger>
                         <Button className="btn" colorScheme='green' bg='white' width='20%'>Enviar</Button>
@@ -63,27 +63,3 @@ import {
   }
   
 
-
-  <Card className="card" align='center' backdropFilter='auto' backdropBlur='40px' ml='2%' mr='2%' border='1px' borderStartRadius='20px' borderEndRadius='20px' variant='outline'>
-  <CardBody >
-    
-    <Stack mt='6' spacing='3' textAlign='center'>
-    <Center>
-                <Heading size="md" fontSize="3.5rem" mt='10' color="white">
-                    Recuperação de Senha
-                    <br /> <br />                 
-                </Heading>
-            </Center>
-
-            <Center>
-                
-                <Stack className="stack" spacing={3}  width='50%'>
-                        <Input placeholder='E-mail' type='email' width='50%'/>    
-                        <Button className="btn" colorScheme='green' bg='white' width='20%'>Enviar</Button>
-                </Stack>
-
-            </Center>
-      
-    </Stack>
-  </CardBody>
-</Card>   
