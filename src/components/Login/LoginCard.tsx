0fbbb7f84f83c039c32cc5249bd2a8c017f86bb1
Felipe Dias <pdias.felipe@gmail.com>
2023-01-17 18:32:32 -0300

    fix: alterando hover dos botões de login

--- src/components/Login/LoginCard.tsx
+++ src/components/Login/LoginCard.tsx
@@ -38,10 +38,10 @@ export function LoginCard() {
 
         <Stack className="stack_b">
           <ButtonGroup className="btn">
-            <Button  bg="white">
+            <Button colorScheme="green" bg="white">
               Entrar
             </Button>
-            <Button  bg="white">
+            <Button colorScheme="green" bg="white">
               <LinkRoute to="/Cadastro">Cadastre-se</LinkRoute>
             </Button>
           </ButtonGroup>
