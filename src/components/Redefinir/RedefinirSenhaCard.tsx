704e89b022ef1e83f3ca2408604432651600ea10
Felipe Dias <pdias.felipe@gmail.com>
2023-01-18 17:10:19 -0300

    fix: RedefinirSenhaCard. Alterei as classes, css e add classes do chakra

--- src/components/Redefinir/RedefinirSenhaCard.tsx
+++ src/components/Redefinir/RedefinirSenhaCard.tsx
@@ -18,23 +18,24 @@ import './style.css'
 
 export function RedefinirSenhaCard() {
     return (
-        <Card className="cadastro_card" backdropFilter='auto' backdropBlur='40px' ml='2%' mr='2%' border='1px' borderStartRadius='20px' borderEndRadius='20px' variant='outline'>
-            <Center>
-                <Heading size="md" fontSize="3.5rem" mt='10' color="white">
+        <Card align='center' className="redefinir-senha-card" backdropFilter='auto' backdropBlur='40px' ml='2%' mr='2%' border='1px' borderStartRadius='20px' borderEndRadius='20px' variant='outline'>
+           
+                <Heading fontSize="3.5rem" mt='6' color="white" >
                     Redefinir Senha
                     <br /> <br />
-                </Heading>
-            </Center>
+                </Heading>          
 
-            <Center>
-
-                <Stack className="stack" spacing={3} width='50%'>
-                    <Input placeholder='Nova Senha' type='email' width='50%' />
-                    <Input placeholder='Repetir Nova Senha' type='email' width='50%' />
-                    <Button className="btn" colorScheme='green' bg='white' width='20%'>Enviar</Button>
+                <Stack className="redefinir-senha-inputs" spacing={3} >
+                    <Input placeholder='Nova Senha' type='password'  />
+                    <Input placeholder='Repetir Nova Senha' type='password' />
                 </Stack>
 
-            </Center>
+                <Stack>
+                <Button mt='6' mb='6' className="btn" colorScheme='green' bg='white'>Enviar</Button>
+                </Stack>
+                
+                
+
         </Card>
     )
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,17 +3,24 @@ import logo from "../assets/imgs/morro-2.png";
 import { Box } from "@chakra-ui/react";
 import { LoginCard } from "../components/Login/LoginCard";
 import { CadastroCard } from "../components/Cadastro/CadastroCard";
+import { ContatoCard } from "../components/Contato/ContatoCard";
+import { DashboardCard } from "../components/Dashboard/DashboardCard";
+import { HomeCard } from "../components/Home/HomeCard";
+import { MetalCard } from "../components/Metal/MetalCard";
+import { ObrigadoCard } from "../components/ObrigadoPorCadastrar/ObrigadoCard";
+import { PapelCard } from "../components/Papel/PapelCard";
+import { ParceirosCard } from "../components/Parceiros/ParceirosCard";
+import { PlasticoCard } from "../components/Plastico/PlasticoCard";
+import { ReciclagemCard } from "../components/Reciclagem/ReciclagemCard";
 import { RecuperarSenhaCard } from "../components/RecuperarSenha/RecuperarSenhaCard";
 import { RedefinirSenhaCard } from "../components/Redefinir/RedefinirSenhaCard";
-import { DashboardCard } from "../components/Dashboard/DashboardCard";
-import { ContatoCard } from "../components/Contato/ContatoCard";
-import { ObrigadoCard } from "../components/ObrigadoPorCadastrar/ObrigadoCard";
+
 function Login() {
   return (
   
      <Box backgroundSize='cover' bgImage={logo} height="100vh">
     <Header />
-    <RecuperarSenhaCard />
+    <RedefinirSenhaCard />
     
     </Box>
 
