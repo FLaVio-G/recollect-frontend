b9f9dbb13875e8d76a7b5bd03057351e836215eb
Felipe Dias <pdias.felipe@gmail.com>
2023-01-18 15:14:57 -0300

    fix: alterações nas classes e css do DAshboardCard

--- src/components/Dashboard/DashboardCard.tsx
+++ src/components/Dashboard/DashboardCard.tsx
@@ -20,15 +20,14 @@ import {
     PopoverTrigger
   } from "@chakra-ui/react";
   
-  import forest from "../../assets/imgs/home-forest.png";
   import './style.css'
 
   
   export function DashboardCard(){
       return(
-        <Card className="cadastro_card" backdropFilter='auto' backdropBlur='40px' ml='2%' mr='2%' border='1px' borderStartRadius='20px' borderEndRadius='20px' variant='outline'>
+        <Card className="dashboard-card" backdropFilter='auto' backdropBlur='40px' ml='2%' mr='2%' border='1px' borderStartRadius='20px' borderEndRadius='20px' variant='outline'>
             <Center>
-            <Heading size="md" fontSize="3.5rem" mt='10' color="white">
+            <Heading fontSize="3.5rem" mt='6' color="white">
                 Dashboard da Empresa
                 <br /> <br />                 
               </Heading>
@@ -37,7 +36,7 @@ import {
             <Center>
                 <Flex>
 
-                    <Stack className='stack_a' spacing={3}  width='100%'>
+                    <Stack className="coluna-nome-da-empresa-dashboard-card" spacing={3}  width='100%'>
                     <Input placeholder='Nome da empresa*' type='name'/> 
                     <Input placeholder='Nome do responsável*' type='name'/>
                     <Input placeholder='E-mail da empresa*' type='name'/>
@@ -46,14 +45,14 @@ import {
                     <Input placeholder='E-mail para login*' type='email'/>
                     </Stack>
 
-                    <Stack className="stack_b" spacing={3}  width='100%'>
+                    <Stack className="coluna-site-da-empresa-dashboard-card" spacing={3}  width='100%'>
                         <Stack className="stack_b1" spacing={3}>
                                 <Input placeholder='Site da empresa' type='name'/> 
                                 <Input placeholder='Telefone do responsável' type='name'/>
                                 <Input placeholder='Endereço da empresa*' type='name'/> 
                         </Stack>
 
-                        <Stack className='stack_b2' spacing={3}>
+                        <Stack spacing={3}>
                             <Flex>
                                 <Input placeholder='CEP*' type='name' width='33%'/> 
                                 <Input placeholder='Cidade*' type='name' width='33%'/>
@@ -61,17 +60,17 @@ import {
                             </Flex>          
                         </Stack>
 
-                        <Stack className="stack_b3" spacing={3}>
+                        <Stack spacing={3}>
                                 <Input placeholder='Retira o material em outros endereços?' type='name'/> 
                             <Flex>
                                 <Input placeholder='Senha' type='password' width='50%'/>
                                 <Input placeholder='Repetir Senha' type='password' width='50%'/>
                             </Flex>
                         </Stack>
-                        <Stack className="btn">
+                        <Stack className="botao-atualizar-dashboard-card">
                         <Popover>
                         <PopoverTrigger>
-                        <Button colorScheme='green' bg='white'>Atualizar</Button>
+                        <Button colorScheme='green' bg='white' color='black'>Atualizar</Button>
                         </PopoverTrigger>
                         <PopoverContent>
                             <PopoverArrow />
