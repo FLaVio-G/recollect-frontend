ce7f61c26fe42f98569a527ce6390c6ba21f9437
Felipe Dias <pdias.felipe@gmail.com>
2023-01-19 19:07:47 -0300

    fix: Contato page

--- src/pages/Cadastro.tsx
+++ src/pages/Cadastro.tsx
@@ -2,13 +2,12 @@ import { Header } from "../components/Header/Header";
 import logo from "../assets/imgs/morro-2.png";
 import { Box } from "@chakra-ui/react";
 import { CadastroCard } from "../components/Cadastro/CadastroCard";
-import { DashboardCard } from "../components/Dashboard/DashboardCard";
 
 function Cadastro() {
   return (
     <Box backgroundSize='cover' bgImage={logo} height="100vh">
       <Header />
-      <DashboardCard />
+      <CadastroCard />
     </Box>
   );
 }
