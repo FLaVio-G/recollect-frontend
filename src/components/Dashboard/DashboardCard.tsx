5f73305c0b5493d1900e905cbc0590c283c75ab6
Felipe Dias <pdias.felipe@gmail.com>
2023-01-20 22:58:09 -0300

    feat: dashboard card apresentando dados da empresa cadastrada

--- src/components/Dashboard/DashboardCard.tsx
+++ src/components/Dashboard/DashboardCard.tsx
@@ -21,9 +21,15 @@ import {
 } from "@chakra-ui/react";
 import { Link as LinkRoute } from "react-router-dom";
 import './style.css'
-
+import {Company} from "../../model/company.type"
+import React, { useState } from 'react';
 
 export function DashboardCard() {
+    const jwt = localStorage.getItem('token') || ''
+    const payload  = JSON.parse(window.atob(jwt.split('.')[1]))
+    const [company, setCompany] = useState<Company>(payload.company);
+    console.log(company)
+    
     return (
         <Card className="dashboard-card"
             backdropFilter='auto'
@@ -44,31 +50,31 @@ export function DashboardCard() {
             <Center>
                 <Flex>
                     <Stack className="coluna-nome-da-empresa-dashboard-card" spacing={3} mt={["-5", '-10']} width='100%'>
-                        <Input placeholder='Nome da empresa*' type='name' />
-                        <Input placeholder='Nome do responsável*' type='name' />
-                        <Input placeholder='E-mail da empresa*' type='name' />
-                        <Input placeholder='Telefone da empresa*' type='name' />
-                        <Input placeholder='Tipos de material que recicla?' type='name' />
-                        <Input placeholder='E-mail para login*' type='email' />
+                        <Input placeholder='Nome da empresa*' value={company.companyName} type='name' />
+                        <Input placeholder='Nome do responsável*' value={company.responsibleName} type='name' />
+                        <Input placeholder='E-mail da empresa*' value={company.companyEmail} type='name' />
+                        <Input placeholder='Telefone da empresa*' value={company.phone} type='name' />
+                        <Input placeholder='Tipos de material que recicla?' value={company.typesOfMaterialYouRecycle} type='name' />
+                        <Input placeholder='E-mail para login*' type='email' value={company.loginEmail} />
                     </Stack>
 
                     <Stack className="coluna-site-da-empresa-dashboard-card" spacing={3} mt={["-5", '-10']} width='100%'>
                         <Stack className="stack_b1" spacing={3}>
-                            <Input placeholder='Site da empresa' type='name' />
-                            <Input placeholder='Telefone do responsável' type='name' />
-                            <Input placeholder='Endereço da empresa*' type='name' />
+                            <Input placeholder='Site da empresa' value={company.site} type='name' />
+                            <Input placeholder='Telefone do responsável' value={company.responsiblePhone} type='name' />
+                            <Input placeholder='Endereço da empresa*' value={company.address?.street} type='name' />
                         </Stack>
 
                         <Stack spacing={3}>
                             <Flex flexDirection={['row', 'row']}>
-                                <Input placeholder='CEP*' type='name' width={['100%', '33%']} />
-                                <Input placeholder='Cidade*' type='name' width={['100%', '33%']} />
-                                <Input placeholder='Estado*' type='name' width={['100%', '33%']} />
+                                <Input placeholder='CEP*' type='name' value={company.address?.zip} width={['100%', '33%']} />
+                                <Input placeholder='Cidade*' type='name' value={company.address?.city} width={['100%', '33%']} />
+                                <Input placeholder='Estado*' type='name' value={company.address?.state} width={['100%', '33%']} />
                             </Flex>
                         </Stack>
 
                         <Stack spacing={3}>
-                            <Input placeholder='Retira o material em outros endereços?' type='name' />
+                            <Input placeholder='Retira o material em outros endereços?' value={company.removeTheMaterialAtAnotherAddress} type='name' />
                         </Stack>
 
                         <Stack className="botao-alterar-e-atualizar-dashboard-card">
@@ -94,3 +100,5 @@ export function DashboardCard() {
         </Card>
     )
 }
+
+
